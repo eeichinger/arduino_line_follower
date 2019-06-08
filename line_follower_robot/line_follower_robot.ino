56233c34e53ea2082918356fae349711fda36e52
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-06-08 13:42:55 +0100

    last version before trying PID

--- line_follower_robot/line_follower_robot.ino
+++ line_follower_robot/line_follower_robot.ino
@@ -3,12 +3,20 @@
 
 const int PIN_SENSOR_LEFT = 13;
 const int PIN_SENSOR_RIGHT = 11;
+const int PIN_SENSOR_FRONT = 5;
+const int PIN_MODE = 3;
 const int PIN_POT_SENSITIVITY = A2;
 
 DualTB9051FTGMotorShield md;
+int mode = 0;
+const int maxTolerance=10;
+int whiteCount = 0;
 
 void stopIfFault()
 {
+  // in debug mode
+  if (mode == 1) return;
+
   if (md.getM1Fault())
   {
     Serial.println("M1 fault");
@@ -16,6 +24,7 @@ void stopIfFault()
     //    while(1);
     delay(5000);
     md.enableDrivers();
+    return;
   }
   if (md.getM2Fault())
   {
@@ -28,11 +37,29 @@ void stopIfFault()
   }
 }
 
+void setSpeed(int m1, int m2) {
+  if (mode == 1) {
+    Serial.print("DBG:");
+    // debug
+    md.setM1Speed(0);
+    md.setM2Speed(0);
+  } else {
+    md.setM1Speed(m1);
+    md.setM2Speed(m2);
+  }
+  Serial.print("M1=");
+  Serial.print(m1);
+  Serial.print(", M2=");
+  Serial.print(m2);
+}
+
 void setup() {
   Serial.begin(19200);
   // put your setup code here, to run once:
   pinMode(PIN_SENSOR_LEFT, INPUT_PULLUP);
   pinMode(PIN_SENSOR_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_FRONT, INPUT_PULLUP);
+  pinMode(PIN_MODE, INPUT_PULLUP);
   pinMode(PIN_POT_SENSITIVITY, INPUT);
 
   md.init();
@@ -43,10 +70,8 @@ void setup() {
   delay(1); // wait for drivers to be enabled so fault pins are no longer low
 }
 
-int blackcount = 0;
-
 void loop() {
-//  delay(5);
+  //  delay(10);
   stopIfFault();
 
   //  int pot_sensitivity = analogRead(PIN_POT_SENSITIVITY);
@@ -55,58 +80,38 @@ void loop() {
   //  md.setM2Speed(speed);
   //  return;
 
+  mode = digitalRead(PIN_MODE);
   int sens_left = digitalRead(PIN_SENSOR_LEFT);
   int sens_right = digitalRead(PIN_SENSOR_RIGHT);
+  int sens_front = digitalRead(PIN_SENSOR_FRONT);
   int pot_sensitivity = analogRead(PIN_POT_SENSITIVITY);
-  int speed = map(pot_sensitivity, 0, 1023, 0, 400); // driver goes up to -400/400
+  int speed = -map(pot_sensitivity, 0, 1023, 0, 400); // driver goes up to -400/400
+
   Serial.println("");
   if ( sens_left == 1 && sens_right == 1 ) {
-    // stop
-    blackcount++;
-    if (blackcount > 4) {
-      blackcount = 4;
-      md.setM1Speed(0);
-      md.setM2Speed(0);
-      Serial.print("M1=000, M2=000");
-    } else {
-      md.setM1Speed(speed);
-      md.setM2Speed(speed);
-      Serial.print("M1=");
-      Serial.print(speed);
-      Serial.print(", M2=");
-      Serial.print(speed);
-    }
+    // go straight and see where it leads
+    setSpeed(speed, speed);
   } else if ( sens_left == 0 && sens_right == 0 ) {
-    blackcount = 0;
     // go straight
-    md.setM1Speed(speed);
-    md.setM2Speed(speed);
-    Serial.print("M1=");
-    Serial.print(speed);
-    Serial.print(", M2=");
-    Serial.print(speed);
+    if (sens_front == 0 && whiteCount > maxTolerance) {
+      // stop, we've lost the line
+      speed = 0;
+    }
+    setSpeed(speed, speed);
   } else if ( sens_left == 1 && sens_right == 0 ) {
-    blackcount = 0;
     // turn left
-    md.setM1Speed(speed*1.25);
-    md.setM2Speed(speed/6);
-    Serial.print("M1=");
-    Serial.print(speed);
-    Serial.print(", M2=");
-    Serial.print(0);
+    setSpeed(speed, speed / 8);
   } else if ( sens_left == 0 && sens_right == 1 ) {
-    blackcount = 0;
     // turn right
-    md.setM1Speed(speed/6);
-    md.setM2Speed(speed*1.25);
-    Serial.print("M1=");
-    Serial.print(0);
-    Serial.print(", M2=");
-    Serial.print(speed);
+    setSpeed(speed / 8, speed);
   }
+  if (sens_front == 0 && sens_left == 0 && sens_right==0) {
+    whiteCount++;    
+  } else {
+    whiteCount=0;
+  }
+  
 
-  Serial.print(", blackcount:");
-  Serial.print(blackcount);
   Serial.print(", left:");
   Serial.print(sens_left);
   Serial.print(", right:");
